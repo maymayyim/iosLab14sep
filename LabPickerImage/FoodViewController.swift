20983e061dea55b140ad448eeae148bc606ed18b
maymayyim <mayyim.putti@gmail.com>
2017-09-14 12:08:03 +0700

    iOS 14 sep

---
+++ LabPickerImage/FoodViewController.swift
@@ -0,0 +1,43 @@
+//
+//  FoodViewController.swift
+//  LabPickerImage
+//
+//  Created by 6272 on 9/14/2560 BE.
+//  Copyright © 2560 6272. All rights reserved.
+//
+
+import UIKit
+
+class FoodViewController: UIViewController {
+    var pName: String = ""
+    @IBOutlet weak var resultdata: UILabel!
+    @IBOutlet weak var foodImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        resultdata.text = pName
+        foodImage.image = UIImage(named: pName+".jpeg")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backMethod(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
